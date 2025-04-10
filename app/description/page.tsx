7963bff7eaf412dc63f206ef51fa17ed73f75d85
Reetika Chavan <reetikachavan1016@gmail.com>
2025-04-10 15:58:35 +0530

    hosting error solved

--- app/description/page.tsx
+++ app/description/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from "react"
 import DescriptionPage from "@/components/dashBoard/description";
 
-export default function ResultPageRoute() {
+export default function DescriptionPageWrapper() {
   return (
-    <DescriptionPage />
-  );
+    <Suspense fallback={<div>Loading...</div>}>
+      <DescriptionPage />
+    </Suspense>
+  )
 }
